c03760b8aa8c816a56dfcdcee3a4cb8e20ef8322
Arti Goldmann <zolotov.artemiy@gmail.com>
2024-08-25 19:12:48 +0300

    Two new tests added

--- Inc/AllocatorTest.h
+++ Inc/AllocatorTest.h
@@ -1,11 +1,12 @@
 #pragma once
 #include "main.h"
 
-enum Index {
+enum IndexDir {
 	fromStart,
 	fromEnd
 };
 
 void Test_max_alloc(Allocator& allocator, size_t& alloc_chunks_counter, std::vector<char*>& allocated);
-void Test_alloc_and_free(std::string test_name, Index start_index);
-void Test_alloc_and_free_through_one(std::string test_name, Index start_index);
+void Test_alloc_and_free(std::string test_name, IndexDir start_index);
+void Test_alloc_and_free_through_one(std::string test_name, IndexDir start_index);
+void Test_alloc_and_free_through_one_v2(std::string test_name, IndexDir start_index);
--- Src/main.cpp
+++ Src/main.cpp
@@ -15,6 +15,9 @@ int main() {
 	//Выделим максимальное кол-во блоков, а потом будем освобождать через один с конца или с начала
 	Test_alloc_and_free_through_one("TEST 3", fromStart);
 	Test_alloc_and_free_through_one("TEST 4", fromEnd);
+	//Выделим максимальное кол-во блоков, освободим блоки через один, потом опять выделим все
+	Test_alloc_and_free_through_one_v2("Test 5", fromStart);
+	Test_alloc_and_free_through_one_v2("Test 6", fromEnd);
 
 	return 0;
 }
